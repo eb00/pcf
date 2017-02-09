41953a6f3774d8a1bb6f75d4034b58123257efd9
Eric Bonnet <eric.d.bonnet@gmail.com>
2017-02-09 17:59:43 +0100

    initial commit

---
+++ simple_pipe.py
@@ -0,0 +1,95 @@
+from subprocess import check_output
+import re
+from time import sleep
+
+class ClusterJob(object):
+
+    bridge_headers = {
+        'project': '#MSUB -A',
+        'ncores': '#MSUB -c',
+        'error_file': '#MSUB -e',
+        'ntasks': '#MSUB -n',
+        'nnodes': '#MSUB -N',
+        'output_file': '#MSUB -o',
+        'queue': '#MSUB -q',
+        'time': '#MSUB -T'
+    }
+
+    shell_header = '#!/bin/bash'
+    shell_cmd = []
+    job_headers = []
+    job_name = 'cmd' 
+    job_id = '0'
+    
+    def __init__(self, **kwargs):
+        
+        headers_dict = self.bridge_headers 
+       
+        # set job name (=shell script name)
+        try:
+            if kwargs['job_name']:
+                self.job_name = kwargs['job_name'] 
+        except:
+            pass
+
+        # set shell commands for the job
+        try:
+            if kwargs['cmd']:
+                for line in kwargs['cmd']:
+                    self.shell_cmd.append(line)
+        except:
+            pass
+
+        # set multiple commands with the ccc_mprun prefix + wait final
+        try:
+            if kwargs['msub']:
+                for cmd in kwargs['msub']:
+                    cmd_line = 'ccc_mprun -n1 bash -c "' + cmd + '" &'
+                    self.shell_cmd.append(cmd_line)
+                self.shell_cmd.append('wait')
+        except:
+            pass
+
+
+        # set job options passed in arguments
+        for k,v in kwargs.iteritems():
+            try:
+                self.job_headers.append(headers_dict[k] + ' ' + v) 
+            except:
+                pass
+        
+    def submit(self):
+
+        # build shell file 
+        fh = open(self.job_name, 'w')
+        fh.write(self.shell_header + '\n')
+        for line in self.job_headers:
+            fh.write(line + '\n')
+        for line in self.shell_cmd:
+            fh.write(line + '\n')
+        fh.close()
+
+        # submit job and get job number
+        out = check_output('bash %s' % self.job_name, shell = True)
+        match = re.search('(\d+)', out)
+        if match:
+            self.job_id = match.group(1)
+
+    # monitor if job is in the queue, i.e. basically pending or running
+    def monitor(self):
+
+        while( __check_cluster_queue == True):
+            sleep(10)
+
+    def __check_cluster_queue(self):
+        ret = False
+        out = check_output('squeue', shell = true)
+        for line in out.split('\n'):
+            if self.job_id == line.strip().split()[0]:
+                ret = True
+                break
+        return ret
+ 
+
+
+
